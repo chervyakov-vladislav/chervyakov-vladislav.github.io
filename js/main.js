ca81cbaf3412f2c2e3b34ceb27a0fc4acc6c2aaf
Vladislav Chervyakov <chervyakovve@mail.ru>
2019-08-31 15:51:34 +0700

    logo fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const burgerButton = document.querySelector('.hamburger-menu-link');
 const burgerStyle = document.querySelector('.hamburger');
-const burgerClose = document.querySelector('.hamburger__menu-close')
+const burgerClose = document.querySelector('.hamburger__menu-close');
+const burgerLogo = document.querySelector('.logo');
 
 burgerButton.addEventListener('click', function(e) {
     burgerStyle.style.display = 'flex';
